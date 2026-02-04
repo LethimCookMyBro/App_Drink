b18240af0ac2ef703f89b76af75843c970ea85ff
lobobobo <winforgitwork@gmail.com>
2026-02-04 22:35:32 +0700

    fix worker-src

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const securityHeaders = [
     value:
       "default-src 'self'; " +
       "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+      "worker-src 'self' blob:; " +
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
       "font-src 'self' https://fonts.gstatic.com data:; " +
       "img-src 'self' data: blob:; " +
